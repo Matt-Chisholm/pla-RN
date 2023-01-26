46f198fbfd36b04818e8f33720e37aaa1ad7107e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-26 10:37:06 -0800

    fleshing out api

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import NewsApi from "../api/NewsApi";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.homeContainer}>
+      <Text style={styles.header}>Explore</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({});
