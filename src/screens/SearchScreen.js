90a46a47c2015262e48afaaf83d7ec6796188e6c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-27 12:21:15 -0800

    left for today

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,38 @@
 import { View, StyleSheet } from "react-native";
-import React, { useState } from "react";
-import { Searchbar, Text, Card } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import {
+  Searchbar,
+  Text,
+  Button,
+  ProgressBar,
+  Avatar,
+  List,
+  Card,
+} from "react-native-paper";
+import axios from "axios";
+import { PLANT_KEY } from "@env";
 
 export default function SearchScreen() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadResults = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://trefle.io/api/v1/plants/search?token=${PLANT_KEY}&q=${searchQuery}`
+      );
+      console.log(response.data.data);
+      setResults(response.data.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError(true);
+      setLoading(false);
+    }
+  };
 
   return (
     <View style={styles.searchContainer}>
@@ -13,6 +41,33 @@ export default function SearchScreen() {
         onChangeText={setSearchQuery}
         value={searchQuery}
       />
+      <Button
+        mode='contained'
+        onPress={() => {
+          loadResults();
+        }}>
+        Search
+      </Button>
+      {loading && <ProgressBar indeterminate />}
+      {error && <Text>Error</Text>}
+      {results.map((item) => (
+        <View style={styles.resultsContainer}>
+          <Card key={item.id}>
+            <Card.Title
+              title={item.common_name}
+              subtitle={item.scientific_name}
+              left={(props) => (
+                <Avatar.Image
+                  {...props}
+                  source={{
+                    uri: item.image_url,
+                  }}
+                />
+              )}
+            />
+          </Card>
+        </View>
+      ))}
     </View>
   );
 }
@@ -20,6 +75,15 @@ export default function SearchScreen() {
 const styles = StyleSheet.create({
   searchContainer: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  resultsContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "80%",
     justifyContent: "center",
     alignItems: "center",
   },
